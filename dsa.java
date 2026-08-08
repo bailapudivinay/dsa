b87884c0b7ae2c881f40d621fb377c34b9d327a5
bailapudivinay <vv6052938@gmail.com>
2026-08-08 19:04:00 +0530

    Refactor Helloworld class and add Friends class

--- dsa.java
+++ dsa.java
@@ -1,8 +1,29 @@
-class Helloworld{
-    public static void main(String[] args){
-        int n = 5;
-        for(int i=0;i<n;i++){
-            System.out.println("HELLO WORLD");
-        }    
+class Friends{
+    String Name;
+    int RollNumber;
+}
+class Main{
+    public static void main(String[] args) {
+        Friends frnd1 = new Friends();
+        frnd1.Name = "LOKESH";
+        frnd1.RollNumber = 9;
+        System.out.println(frnd1.Name);
+        System.out.println(frnd1.RollNumber);
+        Friends frnd2 = new Friends();
+        frnd2.Name = "SHIVA";
+        frnd2.RollNumber = 10;
+        System.out.println(frnd2.Name);
+        System.out.println(frnd2.RollNumber);
     }
+
+    /*  public static void main(String[] args) {
+        int arr[] ={10,20,30,40,50};
+        int sum =0;
+        for(int i=0;i<5;i++){
+            sum = sum + arr[i] ;
+        }
+        System.out.println("Sum of values in array is "+ sum);
+    }   */
+
+
 }

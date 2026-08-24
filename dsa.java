f29d82ee1cd5d003fdd8931b85e10280c6a6afb5
bailapudivinay <vv6052938@gmail.com>
2026-08-24 22:35:59 +0530

    Update dsa.java

--- dsa.java
+++ dsa.java
@@ -38,14 +38,7 @@ class Main{
         System.out.println(best1.goodthings);
     }
 
-    /*  public static void main(String[] args) {
-        int arr[] ={10,20,30,40,50};
-        int sum =0;
-        for(int i=0;i<5;i++){
-            sum = sum + arr[i] ;
-        }
-        System.out.println("Sum of values in array is "+ sum);
-    }   */
+    
 
 
 }

60214c56c43306e1221ebce0920c798be2db4c41
bailapudivinay <vv6052938@gmail.com>
2026-08-25 22:04:31 +0530

    Add new string variable 'b' and print it

--- dsa.java
+++ dsa.java
@@ -1,6 +1,8 @@
 class Main{
     public static void main(String [] args){
         String a = "HELLO" ;
+        String b = "VINAY";
         System.out.println(a);
+        System.out.println(b);
     }
 }

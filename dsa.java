b417ff884bd881e3f2a730e39c48fa5d0b558540
bailapudivinay <vv6052938@gmail.com>
2026-08-27 21:29:37 +0530

    Update dsa.java

--- dsa.java
+++ dsa.java
@@ -1,10 +1,15 @@
-class Main{
-    public static void main(String [] args){
-        String a = "HELLO" ;
-        String b = "VINAY";
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(b);
-        System.out.println(a);
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
     }
 }

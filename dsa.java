e9f3b86b9b9a4bd840321efdac89da1ba67b9e25
bailapudivinay <vv6052938@gmail.com>
2026-08-30 12:13:12 +0530

    Update dsa.java

--- dsa.java
+++ dsa.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int i = 0;
         int j = 0;
-        int k=10;
+        
 
         while (i < s.length() && j < t.length()) {
             if (s.charAt(i) == t.charAt(j)) {

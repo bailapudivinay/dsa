9f7effc718a76b24cec3b984434b8afff399664b
bailapudivinay <vv6052938@gmail.com>
2026-08-31 21:05:22 +0530

    Add variable 'a' in isSubsequence method
    Added a variable 'a' for future use in the isSubsequence method.

--- dsa.java
+++ dsa.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int i = 0;
         int j = 0;
+        int a =0;
         
 
         while (i < s.length() && j < t.length()) {

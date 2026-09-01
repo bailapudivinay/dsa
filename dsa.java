b350dd012e07bff32a46021167c0d8c13e37a0ed
bailapudivinay <vv6052938@gmail.com>
2026-09-01 21:41:42 +0530

    Replace isSubsequence with isUgly method

--- dsa.java
+++ dsa.java
@@ -1,18 +1,22 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int i = 0;
-        int j = 0;
-        int a =0;
-        
+    public boolean isUgly(int n) {
 
-        while (i < s.length() && j < t.length()) {
-            if (s.charAt(i) == t.charAt(j)) {
-                i++;
-            }
-            j++;
-            
+        if (n <= 0) {
+            return false;
         }
 
-        return i == s.length();
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+
+        while (n % 5 == 0) {
+            n = n / 5;
+        }
+
+        return n == 1;
     }
 }

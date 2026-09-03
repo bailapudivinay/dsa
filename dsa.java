532734aff6037fa49b8b505c57675a6be38a784b
bailapudivinay <vv6052938@gmail.com>
2026-09-03 22:13:17 +0530

    Update dsa.java

--- dsa.java
+++ dsa.java
@@ -9,9 +9,7 @@ class Solution {
             n = n / 2;
         }
 
-        while (n % 3 == 0) {
-            n = n / 3;
-        }
+        
 
         while (n % 5 == 0) {
             n = n / 5;

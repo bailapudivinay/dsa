05979fe52c6cd412758cbf94f688a139cc01ea1f
bailapudivinay <vv6052938@gmail.com>
2026-09-11 22:32:14 +0530

    Start implementation of containsDuplicate method

--- dsa.java
+++ dsa.java
@@ -3,8 +3,6 @@ import java.util.HashSet;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-
-        for (int num : nums) {
             
                 
             }
